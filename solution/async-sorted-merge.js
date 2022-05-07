a8986c22045504d9dd979f84107eb6ad8c55841f
saul meneses <fob2257@gmail.com>
2022-05-06 18:25:31 -0700

    Sync/async solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,22 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
+const { sortLogs } = require("./utils");
+
+const getDrainedLog = async (log) => {
+  let { drained } = log;
+  while (!drained) {
+    drained = !(await log.popAsync());
+  }
+  return log;
+};
+
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const drainedLogs = await Promise.all(logSources.map(getDrainedLog));
+    sortLogs(drainedLogs, printer);
+    printer.done();
+
     resolve(console.log("Async sort complete."));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,20 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+const { sortLogs } = require("./utils");
+
+const getDrainedLog = (log) => {
+  let { drained } = log;
+  while (!drained) {
+    drained = !log.pop();
+  }
+  return log;
+};
+
 module.exports = (logSources, printer) => {
+  const drainedLogs = logSources.map(getDrainedLog);
+  sortLogs(drainedLogs, printer);
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
